916d037c0326348de09372fc1f3b72d7b73468f9
Himanshu singh <86113542+Himanshu-Singh7@users.noreply.github.com>
2023-10-12 01:33:07 +0530

    Add files via upload

--- HashMapClasses/HashMap1.java
+++ HashMapClasses/HashMap1.java
@@ -1,5 +1,9 @@
 package HashMapClasses;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 public class HashMap1 {
      public static void main(String[] args) {
 		HashMap<String,Integer> map = new HashMap<>();
@@ -11,7 +15,18 @@ public class HashMap1 {
 		map.put("Python", 3);
 		
 		System.out.println("HashMap : " + map);
-		
+//--------------------------------------------------------------------------		
+//		Set set = map.entrySet();  // HashMap entry convert into set
+//		//System.out.println(set);
+//		Iterator itr = set.iterator();
+//		while(itr.hasNext()) {
+//		Map.Entry entry = (Entry) itr.next();
+//		System.out.println(entry.getKey() +": " + entry.getValue());
+//		}
+//------------------------------------------------------------------------------
+		for(Map.Entry me : map.entrySet()) {
+			System.out.println(me.getKey() + " :"+ me.getValue());
+		}
 		
 		
 	}
